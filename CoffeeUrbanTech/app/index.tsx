85cbf67da76a2a5638173caea8ae08b4b6ddd4ab
ramirezmorac2 <ramirezmorac2@gmail.com>
2025-06-15 17:17:35 -0400

    * fix principal route

--- CoffeeUrbanTech/app/index.tsx
+++ CoffeeUrbanTech/app/index.tsx
@@ -1,17 +1,5 @@
-import { Text, View } from "react-native";
+import DashboardScreen from "./dashboard";
 
 export default function Index() {
-  return (
-    <View style={{ flex: 1 }}>
-      <View
-        style={{
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Text> Hello world!</Text>
-      </View>
-    </View>
-  );
+  return <DashboardScreen />;
 }
